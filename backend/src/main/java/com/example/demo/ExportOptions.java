fe39bfb6215002341f267e712c61715eab0fa2eb
huan yao <bong02bong03@gmail.com>
2025-04-11 22:48:02 +0800

    resolve CE

--- backend/src/main/java/com/example/demo/ExportOptions.java
+++ backend/src/main/java/com/example/demo/ExportOptions.java
@@ -1,60 +1,92 @@
-// package com.example.demo;
-
-// public class ExportOptions {
-//     private ExportFormat format;
-//     private ExportSize size;
-//     private ExportLayout layout;
+package com.example.demo;
+ 
+public class ExportOptions {
+    private ExportFormat format;
+    private ExportSize size;
+    private ExportLayout layout;
+    private Integer customWidth;
+    private Integer customHeight;
     
-//     // Default constructor
-//     public ExportOptions() {
-//     }
+    // Default constructor
+    public ExportOptions() {
+    }
     
-//     // Constructor with all fields
-//     public ExportOptions(ExportFormat format, ExportSize size, ExportLayout layout) {
-//         this.format = format;
-//         this.size = size;
-//         this.layout = layout;
-//     }
+    // Constructor with all fields
+    public ExportOptions(ExportFormat format, ExportSize size, ExportLayout layout) {
+        this.format = format;
+        this.size = size;
+        this.layout = layout;
+    }
     
-//     // Getters and setters
-//     public ExportFormat getFormat() {
-//         return format;
-//     }
+    // Constructor with custom dimensions
+    public ExportOptions(ExportFormat format, ExportSize size, ExportLayout layout, Integer customWidth, Integer customHeight) {
+        this.format = format;
+        this.size = size;
+        this.layout = layout;
+        this.customWidth = customWidth;
+        this.customHeight = customHeight;
+    }
     
-//     public void setFormat(ExportFormat format) {
-//         this.format = format;
-//     }
+    // Getters and setters
+    public ExportFormat getFormat() {
+        return format;
+    }
     
-//     public ExportSize getSize() {
-//         return size;
-//     }
+    public void setFormat(ExportFormat format) {
+        this.format = format;
+    }
     
-//     public void setSize(ExportSize size) {
-//         this.size = size;
-//     }
+    public ExportSize getSize() {
+        return size;
+    }
     
-//     public ExportLayout getLayout() {
-//         return layout;
-//     }
+    public void setSize(ExportSize size) {
+        this.size = size;
+    }
     
-//     public void setLayout(ExportLayout layout) {
-//         this.layout = layout;
-//     }
+    public ExportLayout getLayout() {
+        return layout;
+    }
     
-//     public enum ExportFormat {
-//         JPEG,
-//         PNG
-//     }
+    public void setLayout(ExportLayout layout) {
+        this.layout = layout;
+    }
     
-//     public enum ExportSize {
-//         STANDARD_35x45,
-//         US_PASSPORT_2x2,
-//         CUSTOM
-//     }
+    public Integer getCustomWidth() {
+        return customWidth;
+    }
     
-//     public enum ExportLayout {
-//         SINGLE,
-//         GRID_2x2,
-//         GRID_4x6
-//     }
-// }
+    public void setCustomWidth(Integer customWidth) {
+        this.customWidth = customWidth;
+    }
+    
+    public Integer getCustomHeight() {
+        return customHeight;
+    }
+    
+    public void setCustomHeight(Integer customHeight) {
+        this.customHeight = customHeight;
+    }
+    
+    public enum ExportFormat {
+        JPEG,
+        PNG
+    }
+    
+    public enum ExportSize {
+        STANDARD_35x45,      // Singapore NRIC/Passport (35x45mm)
+        US_PASSPORT_2x2,     // US Passport/Visa (2x2 inch)
+        CHINA_VISA,          // China Visa (33x48mm)
+        MALAYSIA_PASSPORT,   // Malaysia Visa/Passport (35x50mm)
+        AUSTRALIA_VISA,      // Australia Visa (35x45mm)
+        INDIA_PASSPORT,      // Indian Passport/Visa (35x35mm)
+        SMU_ID,              // SMU Student ID
+        CUSTOM               // Custom size
+    }
+    
+    public enum ExportLayout {
+        SINGLE,
+        GRID_2x2,
+        GRID_4x6
+    }
+}
